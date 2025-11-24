8fa399c21bfa440f6c44c7b8b2f138628e6a93b9
Gabriel <gabriel.guzman2201@alumnos.ubiobio.cl>
2025-11-24 04:27:10 -0300

    User Basic CRUD

--- Backend/run.py
+++ Backend/run.py
@@ -1,6 +1,11 @@
-from app import init
+from app import create_app, db
+from app.models import User
 
-app = init.create_app()
+app = create_app()
+
+with app.app_context():
+    db.create_all()  
+    print("Database tables created.")
 
 if __name__ == '__main__':
     app.run(debug=True)
